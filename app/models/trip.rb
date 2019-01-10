6bb478dec22f62f784083e32fbccd305a4276d68
Oliver <omppu842@gmail.com>
2019-01-10 18:56:55 +0200

    Ninja production db edit v2

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -6,5 +6,5 @@ class Trip < ApplicationRecord
   #validates :link, length: {minimum: 10}
   validates :length, numericality: {greater_than_or_equal_to: 500,
                                     less_than_or_equal_to: 2500,
-                                    only_integer: true,}
+                                    only_integer: true}
 end
