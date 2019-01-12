5ee8f6c8b8237dcb227bffadf491d66566b62a63
Oliver <omppu842@gmail.com>
2019-01-12 07:08:04 +0200

    Added basic reversing fuction

--- app/assets/javascripts/triplist.js
+++ app/assets/javascripts/triplist.js
@@ -1,12 +1,29 @@
+const TRIPS = {}
+
+TRIPS.show = () => {
+  const trip_list = []
+
+  TRIPS.list.forEach((trip) =>{
+    trip_list.push('<li>' + trip['start'] +'</li>')
+  })
+
+  $("#trips").html('<ul>' + trip_list.join('') + '</ul>')
+}
+
+TRIPS.reverse = () =>{
+  TRIPS.list.reverse()
+}
+
 document.addEventListener("turbolinks:load", () =>{
+  $("#reverse").click((e) =>{
+    e.preventDefault()
+    TRIPS.reverse()
+    TRIPS.show()
+  })
+
+
   $.getJSON('trips.json', (trips) => {
-    //<!--$("#trips_count").html("matkoja: " + trips.length)-->
-
-    const trip_list = [];
-    trips.forEach( (trip) =>{
-      trip_list.push('<li>' + trip['start'] +'</li>')
-    });
-
-    $("#trips").html('<ul>' + trip_list.join('') + '</ul>')
-  });
+    TRIPS.list = trips
+    TRIPS.show()
+  })
 })
