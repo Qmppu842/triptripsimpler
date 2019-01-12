ca991c5cfffc32d079940c282a5ef60fedcfa4ad
Oliver <omppu842@gmail.com>
2019-01-12 12:04:10 +0200

    Few fonts are now bigger.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :trips
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'trips#index'
+  root 'trips#list'
   get 'arena', to:'trips#arena'
   post 'arena', to:'trips#save_arena_results'
 
