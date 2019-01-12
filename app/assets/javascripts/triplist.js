55242f37e16159fb1db14ec1adb24bc729d864c6
Oliver <omppu842@gmail.com>
2019-01-12 07:00:17 +0200

    Refactory of basic js-listing

--- app/assets/javascripts/triplist.js
+++ app/assets/javascripts/triplist.js
@@ -1,6 +1,12 @@
 document.addEventListener("turbolinks:load", () =>{
   $.getJSON('trips.json', (trips) => {
-    matkat = trips
-    $("#trips").html("matkoja: " + trips.length)
-  })
+    //<!--$("#trips_count").html("matkoja: " + trips.length)-->
+
+    const trip_list = [];
+    trips.forEach( (trip) =>{
+      trip_list.push('<li>' + trip['start'] +'</li>')
+    });
+
+    $("#trips").html('<ul>' + trip_list.join('') + '</ul>')
+  });
 })
