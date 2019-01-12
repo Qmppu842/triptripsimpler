0731565686b1d5ae189d17b652718302c5a1a7f7
Oliver <omppu842@gmail.com>
2019-01-12 12:14:59 +0200

    Lets see if this works on heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :trips
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'trips#list'
+  root 'trips#index'
   get 'arena', to:'trips#arena'
   post 'arena', to:'trips#save_arena_results'
 
