cc690a496091d1ff760857ed80fad4a4cd8437ad
Oliver <omppu842@gmail.com>
2019-01-12 07:57:12 +0200

    Working list ordering with both ascending and descending

--- app/assets/javascripts/triplist.js
+++ app/assets/javascripts/triplist.js
@@ -1,23 +1,79 @@
 const TRIPS = {}
 
 TRIPS.show = () => {
-  const trip_list = []
+  $("#triptable tr:gt(0)").remove()
+  const table = $("#triptable")
+  let isAsc = false
 
   TRIPS.list.forEach((trip) =>{
-    trip_list.push('<li>' + trip['start'] +'</li>')
+    table.append('<tr>'
+    + '<td>' + trip['start'] +'</td>'
+    + '<td>' + trip['end'] +'</td>'
+    + '<td>' + trip['elo'] +'</td>'
+    + '<td>' + trip['length'] +'</td>'
+    + '</tr>')
   })
-
-  $("#trips").html('<ul>' + trip_list.join('') + '</ul>')
 }
 
-TRIPS.reverse = () =>{
-  TRIPS.list.reverse()
+TRIPS.sort_by_start = ()=>{
+    TRIPS.list.sort((a,b) =>{
+      return a.start.toUpperCase().localeCompare(b.start.toUpperCase());
+    })
+}
+TRIPS.sort_by_end = ()=>{
+    TRIPS.list.sort((a,b) =>{
+      return a.end.toUpperCase().localeCompare(b.end.toUpperCase());
+    })
+}
+TRIPS.sort_by_elo = ()=>{
+    TRIPS.list.sort((a,b) =>{
+      return ('' + a.elo).toUpperCase().localeCompare(('' + b.elo).toUpperCase());
+    })
+}
+TRIPS.sort_by_length = ()=>{
+    TRIPS.list.sort((a,b) =>{
+      return ('' + a.length).toUpperCase().localeCompare((''+b.length).toUpperCase());
+    })
+}
+TRIPS.asc = () =>{
+  if (TRIPS.isAsc) {
+    TRIPS.list.reverse()
+    TRIPS.isAsc = false;
+  }else{
+    TRIPS.isAsc = true;
+  }
 }
 
 document.addEventListener("turbolinks:load", () =>{
-  $("#reverse").click((e) =>{
+  if ($("#triptable").length == 0) {
+    return
+  }
+
+  $("#start").click((e) =>{
     e.preventDefault()
-    TRIPS.reverse()
+    TRIPS.sort_by_start()
+    TRIPS.asc();
+    TRIPS.show()
+  })
+
+  $("#end").click((e) =>{
+    e.preventDefault()
+    TRIPS.sort_by_end()
+    TRIPS.asc();
+    TRIPS.show()
+  })
+
+  $("#elo").click((e) =>{
+    e.preventDefault()
+    TRIPS.sort_by_elo()
+    TRIPS.asc();
+    TRIPS.show()
+  })
+
+  $("#length").click((e) =>{
+    e.preventDefault()
+    TRIPS.sort_by_length()
+    TRIPS.asc();
     TRIPS.show()
   })
 
