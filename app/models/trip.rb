2b69902297ee0e86381dba9c3dd4b7dc409e0680
Oliver <omppu842@gmail.com>
2019-01-13 10:31:02 +0200

    First validation test for server

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -4,7 +4,7 @@ class Trip < ApplicationRecord
   validates :link, uniqueness: true,
                    length: {minimum: 10},
                    :if =>lambda{ Rails.env.production?}
-  validates :link, length: {minimum: 10}
+  #validates :link, length: {minimum: 10}
   validates :length, numericality: {greater_than_or_equal_to: 500,
                                     less_than_or_equal_to: 2500,
                                     only_integer: true}
